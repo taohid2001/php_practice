526ebaf9c77d49cd2a04dd76899a67ab573beb66
taohid2001 <smtaohidulislamtanvir03@gmail.com>
2021-08-30 22:39:55 +0600

    php practice part-1

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+echo 'Hello Linux';
+
+$Fname = 'Taohidul Islam ';
+$Lname = 'Tanvir';
+echo "<br/>";
+echo "{$Fname} {$Lname}";
+// Datatype
+echo "<br/>";
+$Int = 12;
+$Flot = 12.12;
+$Bollean = true;
+var_dump($Bollean);
+
+//Arithmetic operators | Assignment operators | Comparison operators | Increment/Decrement operators | Logical operators | String operators | Array operators |Conditional assignment operators
+// Arithmetic operators 
+$Num_1 = 12;
+$Num_2 = 2;
+// $Result = $Num_1 + $Num_2 ;
+// $Result = $Num_1 - $Num_2 ;
+// $Result = $Num_1 * $Num_2 ;
+// $Result = $Num_1 / $Num_2 ;
+// $Result = $Num_1 % $Num_2 ;
+$Result = $Num_1 ** $Num_2 ;
+echo $Result;
+
+?>
